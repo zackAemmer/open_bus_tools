6d7991def6aca2dfb4260a0a6f1f734ce538846e
zackAemmer <zae5op@gmail.com>
2023-10-24 13:53:01 -0700

    Adjusting plots for gps data

--- obt/plot_utils.py
+++ obt/plot_utils.py
@@ -28,15 +28,25 @@ def formatted_lineplot(plot_df, x_var, y_var, title_text):
     plt.savefig(f"{PLOT_FOLDER}{title_text}.png", format='png', dpi=600, bbox_inches='tight')
     return None
 
-def formatted_rel_lineplot(plot_df, x_var, y_var, rel_var, title_text):
+def formatted_rel_lineplot(plot_df, x_var, y_var, rel_var, title_text, xlim=None, ylim=None):
     g = sns.relplot(plot_df, x=x_var, y=y_var, row=rel_var, kind='line', height=HEIGHT_WIDE, aspect=ASPECT_WIDE)
+    if xlim:
+        g.set(xlim=xlim)
+    if ylim:
+        g.set(ylim=ylim)
     plt.savefig(f"{PLOT_FOLDER}{title_text}.eps", format='eps', dpi=600, bbox_inches='tight')
     plt.savefig(f"{PLOT_FOLDER}{title_text}.png", format='png', dpi=600, bbox_inches='tight')
     return None
 
 def formatted_basemap_scatterplot(plot_gdf, title_text):
-    ax = plot_gdf.plot(markersize=5, figsize=(HEIGHT_SQ, WIDTH_SQ))
-    cx.add_basemap(ax, crs=plot_gdf.crs.to_string(), alpha=0.6, source=cx.providers.MapBox(accessToken=os.getenv(key="MAPBOX_TOKEN")))
+    fig, axes = plt.subplots(1,1)
+    fig.set_figheight(HEIGHT_SQ)
+    fig.set_figwidth(WIDTH_SQ)
+    plot_gdf.plot(ax=axes, markersize=5)
+    plot_gdf.iloc[0:1].plot(ax=axes, markersize=100, color='green', marker='x')
+    plot_gdf.iloc[-1:].plot(ax=axes, markersize=100, color='red', marker='x')
+    cx.add_basemap(ax=axes, crs=plot_gdf.crs.to_string(), alpha=0.6, source=cx.providers.MapBox(accessToken=os.getenv(key="MAPBOX_TOKEN")))
+    fig.tight_layout()
     plt.savefig(f"{PLOT_FOLDER}{title_text}.eps", format='eps', dpi=600, bbox_inches='tight')
     plt.savefig(f"{PLOT_FOLDER}{title_text}.png", format='png', dpi=600, bbox_inches='tight')
     return None
