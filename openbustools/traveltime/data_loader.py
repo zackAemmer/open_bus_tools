Add fwd methods, fix label normalization

openbustools/traveltime/data_loader.py
@@ -100,6 +100,8 @@ class DictDataset(Dataset):
         sample_df = self.data_lookup[index]
         sample = {}
         col_idxs = LABEL_FEATS+EMBED_FEATS+GPS_FEATS+STATIC_FEATS+DEEPTTE_FEATS+MISC_CON_FEATS+MISC_CAT_FEATS
+        for i,k in enumerate(LABEL_FEATS):
+            sample[f"{k}_no_norm"] = sample_df[:,col_idxs.index(k)].astype(float)
         for i,k in enumerate(LABEL_FEATS+GPS_FEATS+STATIC_FEATS+DEEPTTE_FEATS+MISC_CON_FEATS):
             sample[k] = normalize(sample_df[:,col_idxs.index(k)].astype(float), self.config[k])
         for k in EMBED_FEATS:
@@ -116,6 +118,7 @@ class DictDataset(Dataset):
 
 def collate(batch):
     y = torch.tensor([b['cumul_time_s'][-1] for b in batch], dtype=torch.float)
+    y_no_norm = torch.tensor([b['cumul_time_s_no_norm'][-1] for b in batch], dtype=torch.float)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em = torch.concat([X_em_mod, X_em_dow], axis=1)
@@ -123,11 +126,12 @@ def collate(batch):
     for i,col in enumerate(GPS_FEATS):
         X_ct[:,i] = torch.tensor([b[col][0] for b in batch], dtype=torch.float)
         X_ct[:,i+len(GPS_FEATS)] = torch.tensor([b[col][-1] for b in batch], dtype=torch.float)
-    return (X_em, X_ct), y
+    return (X_em, X_ct), (y, y_no_norm)
 
 
 def collate_static(batch):
     y = torch.tensor([b['cumul_time_s'][-1] for b in batch], dtype=torch.float)
+    y_no_norm = torch.tensor([b['cumul_time_s_no_norm'][-1] for b in batch], dtype=torch.float)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em = torch.concat([X_em_mod, X_em_dow], axis=1)
@@ -135,11 +139,12 @@ def collate_static(batch):
     for i,col in enumerate(GPS_FEATS+STATIC_FEATS):
         X_ct[:,i] = torch.tensor([b[col][0] for b in batch], dtype=torch.float)
         X_ct[:,i+len(GPS_FEATS)+len(STATIC_FEATS)] = torch.tensor([b[col][-1] for b in batch], dtype=torch.float)
-    return (X_em, X_ct), y
+    return (X_em, X_ct), (y, y_no_norm)
 
 
 def collate_realtime(batch):
     y = torch.tensor([b['cumul_time_s'][-1] for b in batch], dtype=torch.float)
+    y_no_norm = torch.tensor([b['cumul_time_s_no_norm'][-1] for b in batch], dtype=torch.float)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em = torch.concat([X_em_mod, X_em_dow], axis=1)
@@ -152,11 +157,12 @@ def collate_realtime(batch):
     X_gr = torch.zeros((len(batch), grid_channels, grid_n, 2))
     X_gr[:,:,:,0] = torch.concat([torch.tensor(b['grid'][0,:,:], dtype=torch.float).unsqueeze(0) for b in batch], dim=0)
     X_gr[:,:,:,1] = torch.concat([torch.tensor(b['grid'][-1,:,:], dtype=torch.float).unsqueeze(0) for b in batch], dim=0)
-    return (X_em, X_ct, X_gr), y
+    return (X_em, X_ct, X_gr), (y, y_no_norm)
 
 
 def collate_seq(batch):
     y = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s'], dtype=torch.float) for b in batch])
+    y_no_norm = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s_no_norm'], dtype=torch.float) for b in batch])
     X_sl = torch.tensor([len(b['calc_time_s']) for b in batch], dtype=torch.int)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
@@ -164,11 +170,12 @@ def collate_seq(batch):
     X_ct = torch.zeros((torch.max(X_sl), len(batch), len(GPS_FEATS)))
     for i,col in enumerate(GPS_FEATS):
         X_ct[:,:,i] = torch.nn.utils.rnn.pad_sequence([torch.tensor(b[col], dtype=torch.float) for b in batch])
-    return (X_em, X_ct, X_sl), y
+    return (X_em, X_ct, X_sl), (y, y_no_norm)
 
 
 def collate_seq_static(batch):
     y = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s'], dtype=torch.float) for b in batch])
+    y_no_norm = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s_no_norm'], dtype=torch.float) for b in batch])
     X_sl = torch.tensor([len(b['calc_time_s']) for b in batch], dtype=torch.int)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
@@ -176,11 +183,12 @@ def collate_seq_static(batch):
     X_ct = torch.zeros((torch.max(X_sl), len(batch), len(GPS_FEATS+STATIC_FEATS)))
     for i,col in enumerate(GPS_FEATS+STATIC_FEATS):
         X_ct[:,:,i] = torch.nn.utils.rnn.pad_sequence([torch.tensor(b[col], dtype=torch.float) for b in batch])
-    return (X_em, X_ct, X_sl), y
+    return (X_em, X_ct, X_sl), (y, y_no_norm)
 
 
 def collate_seq_realtime(batch):
     y = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s'], dtype=torch.float) for b in batch])
+    y_no_norm = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['calc_time_s_no_norm'], dtype=torch.float) for b in batch])
     X_sl = torch.tensor([len(b['calc_time_s']) for b in batch], dtype=torch.int)
     X_em_dow = torch.tensor([b['t_day_of_week'] for b in batch], dtype=torch.int).unsqueeze(-1)
     X_em_mod = torch.tensor([b['t_min_of_day'] for b in batch], dtype=torch.int).unsqueeze(-1)
@@ -191,7 +199,7 @@ def collate_seq_realtime(batch):
     X_gr = torch.nn.utils.rnn.pad_sequence([torch.tensor(b['grid'], dtype=torch.float) for b in batch], batch_first=True)
     X_gr = torch.swapaxes(X_gr, 1, 3)
     X_gr = torch.swapaxes(X_gr, 1, 2)
-    return (X_em, X_ct, X_sl, X_gr), y
+    return (X_em, X_ct, X_sl, X_gr), (y, y_no_norm)
 
 
 def collate_deeptte(batch):

openbustools/traveltime/models/ff.py
@@ -36,10 +36,7 @@ class FF(pl.LightningModule):
         # Linear compression/feature extraction
         self.feature_extract = torch.nn.Linear(self.hidden_size, 1)
         self.feature_extract_activation = torch.nn.ReLU()
-    def training_step(self, batch):
-        x,y = batch
-        x_em = x[0]
-        x_ct = x[1]
+    def forward(self, x_em, x_ct):
         # Embed categorical variables
         x_min_em = self.min_em(x_em[:,0])
         x_day_em = self.day_em(x_em[:,1])
@@ -47,7 +44,15 @@ class FF(pl.LightningModule):
         out = torch.cat([x_ct, x_min_em, x_day_em], dim=1)
         out = self.linear_relu_stack(out)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
-        loss = self.loss_fn(out, y)
+        return out
+    def training_step(self, batch):
+        x,y = batch
+        x_em = x[0]
+        x_ct = x[1]
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct)
+        loss = self.loss_fn(out, y_norm)
         self.log_dict(
             {'train_loss': loss},
             on_step=False,
@@ -59,14 +64,10 @@ class FF(pl.LightningModule):
         x,y = batch
         x_em = x[0]
         x_ct = x[1]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        # Combine all variables
-        out = torch.cat([x_ct, x_min_em, x_day_em], dim=1)
-        out = self.linear_relu_stack(out)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
-        loss = self.loss_fn(out, y)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct)
+        loss = self.loss_fn(out, y_norm)
         self.log_dict(
             {'valid_loss': loss},
             on_step=False,
@@ -78,15 +79,12 @@ class FF(pl.LightningModule):
         x,y = batch
         x_em = x[0]
         x_ct = x[1]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        # Combine all variables
-        out = torch.cat([x_ct, x_min_em, x_day_em], dim=1)
-        out = self.linear_relu_stack(out)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct)
         out = out.detach().cpu().numpy()
-        return {'preds':out, 'labels':y}
+        y_no_norm = y_no_norm.detach().cpu().numpy()
+        return {'preds':out, 'labels':y_no_norm}
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
@@ -127,11 +125,7 @@ class FFRealtime(pl.LightningModule):
         self.feature_extract_activation = torch.nn.ReLU()
         # Grid
         self.grid_stack = realtime.GridFeedForward(self.grid_input_size, self.grid_compression_size, self.hidden_size)
-    def training_step(self, batch):
-        x,y = batch
-        x_em = x[0]
-        x_ct = x[1]
-        x_gr = x[2]
+    def forward(self, x_em, x_ct, x_gr):
         # Embed categorical variables
         x_min_em = self.min_em(x_em[:,0])
         x_day_em = self.day_em(x_em[:,1])
@@ -142,7 +136,16 @@ class FFRealtime(pl.LightningModule):
         out = torch.cat([x_ct, x_min_em, x_day_em, x_gr], dim=1)
         out = self.linear_relu_stack(out)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
-        loss = self.loss_fn(out, y)
+        return out
+    def training_step(self, batch):
+        x,y = batch
+        x_em = x[0]
+        x_ct = x[1]
+        x_gr = x[2]
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
+        loss = self.loss_fn(out, y_norm)
         self.log_dict(
             {'train_loss': loss},
             on_step=False,
@@ -155,17 +158,10 @@ class FFRealtime(pl.LightningModule):
         x_em = x[0]
         x_ct = x[1]
         x_gr = x[2]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        # Grid
-        x_gr = self.grid_stack(x_gr)
-        x_gr = x_gr.view(x_gr.shape[0], x_gr.shape[1]*x_gr.shape[2])
-        # Combine all variables
-        out = torch.cat([x_ct, x_min_em, x_day_em, x_gr], dim=1)
-        out = self.linear_relu_stack(out)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
-        loss = self.loss_fn(out, y)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
+        loss = self.loss_fn(out, y_norm)
         self.log_dict(
             {'valid_loss': loss},
             on_step=False,
@@ -178,18 +174,12 @@ class FFRealtime(pl.LightningModule):
         x_em = x[0]
         x_ct = x[1]
         x_gr = x[2]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        # Grid
-        x_gr = self.grid_stack(x_gr)
-        x_gr = x_gr.view(x_gr.shape[0], x_gr.shape[1]*x_gr.shape[2])
-        # Combine all variables
-        out = torch.cat([x_ct, x_min_em, x_day_em, x_gr], dim=1)
-        out = self.linear_relu_stack(out)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze()
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
         out = out.detach().cpu().numpy()
-        return {'preds':out, 'labels':y}
+        y_no_norm = y_no_norm.detach().cpu().numpy()
+        return {'preds':out, 'labels':y_no_norm}
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer

openbustools/traveltime/models/rnn.py
@@ -32,11 +32,7 @@ class GRU(pl.LightningModule):
         # Linear compression/feature extraction
         self.feature_extract = nn.Linear(in_features=self.hidden_size + self.embed_total_dims, out_features=1)
         self.feature_extract_activation = nn.ReLU()
-    def training_step(self, batch):
-        x,y = batch
-        x_em = x[0]
-        x_ct = x[1]
-        x_sl = x[2]
+    def forward(self, x_em, x_ct):
         # Embed categorical variables
         x_min_em = self.min_em(x_em[:,0])
         x_day_em = self.day_em(x_em[:,1])
@@ -47,10 +43,19 @@ class GRU(pl.LightningModule):
         # Combine all variables
         out = torch.cat([x_em, x_ct], dim=2)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        return out
+    def training_step(self, batch):
+        x,y = batch
+        x_em = x[0]
+        x_ct = x[1]
+        x_sl = x[2]
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
-        loss = self.loss_fn(out, y, mask)
+        loss = self.loss_fn(out, y_norm, mask)
         self.log_dict(
             {'train_loss': loss},
             on_step=False,
@@ -64,20 +69,13 @@ class GRU(pl.LightningModule):
         x_em = x[0]
         x_ct = x[1]
         x_sl = x[2]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        x_em = torch.cat((x_min_em, x_day_em), dim=1).unsqueeze(0)
-        x_em = x_em.expand(x_ct.shape[0], -1, -1)
-        # Get recurrent pred
-        x_ct, hidden_prev = self.rnn(x_ct)
-        # Combine all variables
-        out = torch.cat([x_em, x_ct], dim=2)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_sl)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
-        loss = self.loss_fn(out, y, mask)
+        loss = self.loss_fn(out, y_norm, mask)
         self.log_dict(
             {'valid_loss': loss},
             on_step=False,
@@ -91,25 +89,18 @@ class GRU(pl.LightningModule):
         x_em = x[0]
         x_ct = x[1]
         x_sl = x[2]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        x_em = torch.cat((x_min_em, x_day_em), dim=1).unsqueeze(0)
-        x_em = x_em.expand(x_ct.shape[0], -1, -1)
-        # Get recurrent pred
-        x_ct, hidden_prev = self.rnn(x_ct)
-        # Combine all variables
-        out = torch.cat([x_em, x_ct], dim=2)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_sl)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
         # Move to cpu and return predictions, labels
         mask = mask.detach().cpu().numpy()
         out = out.detach().cpu().numpy()
-        y = y.detach().cpu().numpy()
+        y_no_norm = y_no_norm.detach().cpu().numpy()
         out_agg = model_utils.aggregate_tts(out, mask)
-        y_agg = model_utils.aggregate_tts(y, mask)
+        y_agg = model_utils.aggregate_tts(y_no_norm, mask)
         return {'preds': out_agg, 'labels': y_agg}
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -145,12 +136,7 @@ class GRURealtime(pl.LightningModule):
         self.feature_extract_activation = nn.ReLU()
         # Grid
         self.grid_stack = realtime.GridFeedForward(self.grid_input_size, self.grid_compression_size, self.hidden_size)
-    def training_step(self, batch):
-        x,y = batch
-        x_em = x[0]
-        x_ct = x[1]
-        x_sl = x[2]
-        x_gr = x[3]
+    def forward(self, x_em, x_ct, x_gr):
         # Embed categorical variables
         x_min_em = self.min_em(x_em[:,0])
         x_day_em = self.day_em(x_em[:,1])
@@ -165,10 +151,20 @@ class GRURealtime(pl.LightningModule):
         # Combine all variables
         out = torch.cat([x_em, x_ct], dim=2)
         out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        return out
+    def training_step(self, batch):
+        x,y = batch
+        x_em = x[0]
+        x_ct = x[1]
+        x_sl = x[2]
+        x_gr = x[3]
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
-        loss = self.loss_fn(out, y, mask)
+        loss = self.loss_fn(out, y_norm, mask)
         self.log_dict(
             {'train_loss': loss},
             on_step=False,
@@ -183,24 +179,13 @@ class GRURealtime(pl.LightningModule):
         x_ct = x[1]
         x_sl = x[2]
         x_gr = x[3]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        x_em = torch.cat((x_min_em, x_day_em), dim=1).unsqueeze(0)
-        x_em = x_em.expand(x_ct.shape[0], -1, -1)
-        # Grid
-        x_gr = self.grid_stack(x_gr)
-        x_gr = torch.swapaxes(x_gr, 0, 1)
-        x_ct = torch.cat([x_ct, x_gr], dim=2)
-        # Get recurrent pred
-        x_ct, hidden_prev = self.rnn(x_ct)
-        # Combine all variables
-        out = torch.cat([x_em, x_ct], dim=2)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
-        loss = self.loss_fn(out, y, mask)
+        loss = self.loss_fn(out, y_norm, mask)
         self.log_dict(
             {'valid_loss': loss},
             on_step=False,
@@ -215,29 +200,18 @@ class GRURealtime(pl.LightningModule):
         x_ct = x[1]
         x_sl = x[2]
         x_gr = x[3]
-        # Embed categorical variables
-        x_min_em = self.min_em(x_em[:,0])
-        x_day_em = self.day_em(x_em[:,1])
-        x_em = torch.cat((x_min_em, x_day_em), dim=1).unsqueeze(0)
-        x_em = x_em.expand(x_ct.shape[0], -1, -1)
-        # Grid
-        x_gr = self.grid_stack(x_gr)
-        x_gr = torch.swapaxes(x_gr, 0, 1)
-        x_ct = torch.cat([x_ct, x_gr], dim=2)
-        # Get recurrent pred
-        x_ct, hidden_prev = self.rnn(x_ct)
-        # Combine all variables
-        out = torch.cat([x_em, x_ct], dim=2)
-        out = self.feature_extract(self.feature_extract_activation(out)).squeeze(2)
+        y_norm = y[0]
+        y_no_norm = y[1]
+        out = self.forward(x_em, x_ct, x_gr)
         # Masked loss; init mask here since module knows device
         mask = torch.zeros(max(x_sl), len(x_sl), dtype=torch.bool, device=self.device)
         mask = model_utils.fill_tensor_mask(mask, x_sl)
         # Move to cpu and return predictions, labels
         mask = mask.detach().cpu().numpy()
         out = out.detach().cpu().numpy()
-        y = y.detach().cpu().numpy()
+        y_no_norm = y_no_norm.detach().cpu().numpy()
         out_agg = model_utils.aggregate_tts(out, mask)
-        y_agg = model_utils.aggregate_tts(y, mask)
+        y_agg = model_utils.aggregate_tts(y_no_norm, mask)
         return {'preds': out_agg, 'labels': y_agg}
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)